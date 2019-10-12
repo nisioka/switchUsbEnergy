6cc5740204228d15596f7b68e0101dbdc44a7fe0
nisioka <nishioka.daisuke@gmail.com>
2019-10-12 19:37:48 +0900

    deal with python2.

--- switchUsbEnergy/main.py
+++ switchUsbEnergy/main.py
@@ -7,8 +7,8 @@ import time
 USB_ENERGY_ON = ["sudo", "hub-ctrl", "-h", "0", "-P", "2", "-p", "1"]
 USB_ENERGY_OFF = ["sudo", "hub-ctrl", "-h", "0", "-P", "2", "-p", "0"]
 
-# def __init__(self):
-#     subprocess.run(USB_ENERGY_OFF)
+def __init__(self):
+    subprocess.call(USB_ENERGY_OFF)
 
 @route('/hello')
 def hello():
@@ -17,9 +17,9 @@ def hello():
 @route('/test')
 def flashUsb():
     for i in range(4):
-        subprocess.run(USB_ENERGY_ON)
+        subprocess.call(USB_ENERGY_ON)
         time.sleep(0.7)
-        subprocess.run(USB_ENERGY_OFF)
+        subprocess.call(USB_ENERGY_OFF)
         time.sleep(0.3)
 
 
