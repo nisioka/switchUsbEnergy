23f77e3b53a8a96d36597c00eb717ef6c09dee4d
nisioka <nishioka.daisuke@gmail.com>
2019-10-13 17:44:06 +0900

    no http server version.

--- switchUsbEnergy/main.py
+++ switchUsbEnergy/main.py
@@ -1,19 +1,13 @@
 # -*- coding:utf-8 -*-
 
-from bottle import route, run
 import subprocess
 import time
 
 USB_ENERGY_ON = ["sudo", "hub-ctrl", "-h", "0", "-P", "2", "-p", "1"]
 USB_ENERGY_OFF = ["sudo", "hub-ctrl", "-h", "0", "-P", "2", "-p", "0"]
 
-@route('/flash')
-def flashUsb():
-    for i in range(4):
-        subprocess.call(USB_ENERGY_ON)
-        time.sleep(0.7)
-        subprocess.call(USB_ENERGY_OFF)
-        time.sleep(0.3)
-
-subprocess.call(USB_ENERGY_OFF)
-run(host='localhost', port=8080, debug=True)
+for i in range(4):
+    subprocess.call(USB_ENERGY_ON)
+    time.sleep(0.7)
+    subprocess.call(USB_ENERGY_OFF)
+    time.sleep(0.3)
